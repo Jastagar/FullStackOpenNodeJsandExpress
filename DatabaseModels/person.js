dcc32443e9fe5fbd134683ac3ebbb59c36887b41
Jastagar <jastagar1581.be20@chitkara.edu.in>
2022-07-28 09:47:42 +0530

    esLint Problems Solved

--- DatabaseModels/person.js
+++ DatabaseModels/person.js
@@ -1,28 +1,31 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
 const mongoose = require('mongoose')
 
 const person = new mongoose.Schema({
-    "name": {
-        type:String,
-        minLength:3,
-        required:true
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
     },
-    "number": {
-        type:String,
+    number: {
+        type: String,
         validate: {
-            validator: function(v) {
-              return /\d{2,3}-\d{10}/.test(v);
+            validator(v) {
+                return /\d{2,3}-\d{10}/.test(v)
             },
-            message: () => `This is not a valid phone number!`
-          },
-        required:true}
+            message: () => 'This is not a valid phone number!',
+        },
+        required: true,
+    },
 })
 
-person.set("toJSON", {
+person.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
-    }
+    },
 })
 
-module.exports = mongoose.model("Persons", person)
+module.exports = mongoose.model('Persons', person)
--- index.js
+++ index.js
@@ -1,50 +1,42 @@
 require('dotenv').config()
-
 const express = require('express')
-const app = express()
-const cors = require("cors")
-const mongoose = require('mongoose')
-const Person = require("./DatabaseModels/person")
 
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const Person = require('./DatabaseModels/person')
 
 function errorHandling(error, req, res, next) {
-    console.error("THis the the error handling midleware")
+    console.error('THis the the error handling midleware')
     console.error(error.message)
     console.error(error.name)
     if (error.name === 'CastError') {
         return res.status(400).send({ error: 'malformatted id' })
-    }else if (error.name === 'ValidationError') {
-        console.log("This was also triggered")
+    }
+    if (error.name === 'ValidationError') {
+        console.log('This was also triggered')
         return res.status(400).json({ error: error.message })
     }
-    next(error)
+    return next(error)
 }
-
-
-app.use(express.json());
+app.use(express.json())
 app.use(cors())
-// app.use(morgan('tiny'))
-app.use(express.static("build"))
-
-
+// app.use(morgan('tiny'));
+app.use(express.static('build'))
 mongoose.connect(process.env.DATABASE_URL)
-    .then((result) => {
-        console.log("Connected to mongoose")
+    .then(() => {
+        console.log('Connected to mongoose')
     })
     .catch((err) => {
-        console.log("Couldn't Connect to mongo Database because of :", err)
+        console.log('Could not Connect to mongo Database because of :', err)
     })
-
-
-
-
-// Request handling.
-app.get("/api/persons", (req, res, next) => {
+// Request handling.;
+app.get('/api/persons', (req, res, next) => {
     Person.find({}).then((result) => {
         res.json(result)
     }).catch((err) => next(err))
 })
-app.get("/info", (req, res, next) => {
+app.get('/info', (req, res, next) => {
     const timeStamp = new Date().toString()
     Person.find({}).then((result) => {
         console.log(result)
@@ -54,8 +46,8 @@ app.get("/info", (req, res, next) => {
         `)
     }).catch((err) => next(err))
 })
-app.get("/api/persons/:id", (req, res, next) => {
-    const id = req.params.id;
+app.get('/api/persons/:id', (req, res, next) => {
+    const { id } = req.params.id
     Person.findById(id).then((foundPerson) => {
         if (foundPerson) {
             res.send(foundPerson)
@@ -64,124 +56,72 @@ app.get("/api/persons/:id", (req, res, next) => {
         }
     }).catch((err) => next(err))
 })
-
-
-app.delete("/api/persons/:id", (req, res, next) => {
-    const id = req.params.id;
+app.delete('/api/persons/:id', (req, res, next) => {
+    const { id } = req.params.id
     Person.findByIdAndDelete(id).then((something) => {
         if (something) {
-            res.send("deleted")
+            res.send('deleted')
         } else {
-            res.send("Not Found, maybe Deleted already")
+            res.send('Not Found, maybe Deleted already')
         }
     }).catch((err) => next(err))
-
 })
-
-app.post("/api/persons", (req, res, next) => {
-    const name = req.body.name
-    const number = req.body.number
-    Person.find({ name: name }, { number: number })
+app.post('/api/persons', (req, res, next) => {
+    const { name } = req.body.name
+    const { number } = req.body.number
+    Person.find({ name }, { number })
         .then((foundPerson) => {
             if (foundPerson.length) {
                 console.log(foundPerson)
-                res.send("Sorry this name or number already exists at id: ",
-                    foundPerson.id)
+                res.send('Sorry this name or number already exists at id: ', foundPerson.id)
             } else {
-                const newContact = new Person({
-                    name: name,
-                    number: number
-                })
+                const newContact = new Person({ name, number })
                 const errorV = newContact.save()
                 next(errorV)
-                
-                res.send("User Added")
+                res.send('User Added')
             }
-        }).catch((err)=>{
-            console.log("Outer Catch was also called")
+        }).catch(() => {
+            console.log('Outer Catch was also called')
         })
 })
-
-app.put("/api/persons/:id",(req,res,next)=>{
+app.put('/api/persons/:id', (req, res, next) => {
     console.log(req.body)
-    Person.findByIdAndUpdate(req.body.id,req.body,{new:true,runValidators: true, context: 'query'}).then((result)=>{
+    Person.findByIdAndUpdate(req.body.id, req.body, { new: true, runValidators: true, context: 'query' }).then((result) => {
         console.log(result)
-    }).catch(err=>next(err))
-    res.send("Recieved")
+    }).catch((err) => next(err))
+    res.send('Recieved')
 })
-
-
 app.use(errorHandling)
-
-
 app.listen(process.env.PORT, () => {
     console.log(process.env.PORT)
 })
-
-
-// Check the middleware, its not being called
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const http = require('http')
-
-// let notes = [
+// const http = require('http');
+// let notes = [;
 //     {
-//       id: 1,
-//       content: "HTML is easy",
-//       date: "2022-05-30T17:30:31.098Z",
-//       important: true
-//     },
+//       id: 1,;
+//       content: 'HTML is easy',;
+//       date: '2022-05-30T17:30:31.098Z',;
+//       important: true;
+//     },;
 //     {
-//       id: 2,
-//       content: "Browser can execute only Javascript",
-//       date: "2022-05-30T18:39:34.091Z",
-//       important: false
-//     },
+//       id: 2,;
+//       content: 'Browser can execute only Javascript',;
+//       date: '2022-05-30T18:39:34.091Z',;
+//       important: false;
+//     },;
 //     {
-//       id: 3,
-//       content: "GET and POST are the most important methods of HTTP protocol",
-//       date: "2022-05-30T19:20:14.298Z",
-//       important: true
-//     }
-//   ]
+//       id: 3,;
+//       content: 'GET and POST are the most important methods of HTTP protocol',;
+//       date: '2022-05-30T19:20:14.298Z',;
+//       important: true;
+//     };
+//   ];
 
 // const app = http.createServer((req,res)=>{
-//     res.writeHead(200,{"content-Type":"application/json"})
-//     res.end(JSON.stringify(notes))
-// })
+//     res.writeHead(200,{'content-Type':'application/json'});
+//     res.end(JSON.stringify(notes));
+// });
 
-// const PORT = 3001
-// app.listen(PORT)
-// console.log("Server running on port",PORT)
+// const PORT = 3001;
+// app.listen(PORT);
+// console.log('Server running on port',PORT)
